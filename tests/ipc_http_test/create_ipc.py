9e1bc43eaf4f7e87b6ea21d88bc753c516c52172
= <huangjh233@outlook.com>
2025-06-18 15:17:38 +0100

    Separation of model saving and computing API

--- tests/ipc_http_test/create_ipc.py
+++ tests/ipc_http_test/create_ipc.py
@@ -3,13 +3,13 @@ import pyarrow as pa
 import requests
 
 # Upload the .mat file to server
-url = "http://127.0.0.1:8000/compute"
+url = "http://127.0.0.1:8000/saveModel"
 mat_file_path = "sample\\e_coli_core.mat"
 files = {
-    'file': ('e_coli_core.mat', open(mat_file_path, 'rb'), 'text/plain')
+    'model': ('e_coli_core.mat', open(mat_file_path, 'rb'), 'text/plain')
 }
 params = {
-    "file_path": mat_file_path
+    "model_name": "e_coli_core"
 }
 req = requests.post(url, files=files, data=params)
 print(req.content)
