19386a54b31d2d2fb388bccdcaae84e564926b4a
yixing <leiyixing@gmail.com>
2025-06-18 17:08:08 +0100

    parser

---
+++ tests/ipc_http_test/parser.py
@@ -0,0 +1,15 @@
+
+import pyarrow as pa
+import requests
+
+# Upload the .mat file to server
+url = "http://127.0.0.1:8000/saveModel"
+mat_file_path = "../sample/e_coli_core.mat"
+files = {
+    'model': ('e_coli_core.mat', open(mat_file_path, 'rb'), 'text/plain')
+}
+params = {
+    "model_name": "e_coli_core"
+}
+req = requests.post(url, files=files, data=params)
+print(req.content)
