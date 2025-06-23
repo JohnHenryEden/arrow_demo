e53bd374be8bd9620c87057248d3fb05cabb11a4
Yixing Lei <leiyixing@gmail.com>
2025-06-23 15:27:10 +0100

    Bugfix/save model optional

--- tests/ipc_http_test/parser.py
+++ tests/ipc_http_test/parser.py
@@ -3,18 +3,19 @@ import pyarrow as pa
 import pyarrow.compute as pc
 import requests
 
-test_array = pa.array([1, 2, 3, 4, 5], type=pa.int64())
-max_value = pc.max(test_array)
-print(f"Max value in test_array: {max_value.as_py()}")
+# test_array = pa.array([1, 2, 3, 4, 5], type=pa.int64())
+# max_value = pc.max(test_array)
+# print(f"Max value in test_array: {max_value.as_py()}")
 
 # Upload the .mat file to server
 url = "http://127.0.0.1:8000/saveModel"
-mat_file_path = "../sample/e_coli_core.mat"
+mat_file_path = "/Users/yixing/Documents/recon4imd/2025/oprarrow/sample/e_coli_core.mat"
 files = {
-    'model': ('e_coli_core.mat', open(mat_file_path, 'rb'), 'text/plain')
+    'model': ('e_coli_core.mat', open(mat_file_path, 'rb'),  "application/octet-stream")
 }
 params = {
-    "model_name": "e_coli_core"
+    "model_name": "e_coli_core",
+    "model_id": "123",
 }
 req = requests.post(url, files=files, data=params)
 print(req.content)
